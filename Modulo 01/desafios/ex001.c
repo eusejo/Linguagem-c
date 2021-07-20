9387a42d5589649559338cc44a7e0e3abcf31243
euyag <clancysla@gmail.com>
2021-07-20 18:50:07 -0300

    coisas adicionais
    readme e um ex novo

--- Modulo 01/desafios/ex001.c
+++ Modulo 01/desafios/ex001.c
@@ -2,10 +2,10 @@
 #include <locale.h>
 
 void main(){
-    setlocale(0, "Portuguese")
+    setlocale(0, "Portuguese");
     printf("Listagem de Alunos\n");
     printf("Nome \t\tNota\n");
     printf("---------------------\n");
     printf("Ana Beatriz\t 8.5 \nBianca Martins\t 9.0\n");
-    printf("Cláudio sá\t 5.5 \nGiovana Silva\t 7.5");
+    printf("Claudio sa\t 5.5 \nGiovana Silva\t 7.5");
 }
